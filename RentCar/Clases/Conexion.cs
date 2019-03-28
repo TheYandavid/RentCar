900c2491ff76aece82a2ffeb9e46bb9f2fec99c0
TheYandavid <16311631+TheYandavid@users.noreply.github.com>
2019-03-27 22:29:05 -0400

    Completo 0.3

---
+++ RentCar/Clases/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace RentCar.Clases
+{
+     class Conexion
+    {
+        private static void SqlConexion()
+        {
+            SqlConnection con;
+             con = new SqlConnection("Data Source=DESKTOP-7UG5AJD\\SQLEXPRESS02;Initial Catalog=RentCar;Integrated Security=True");
+        }
+        
+      
+
+    }
+}
